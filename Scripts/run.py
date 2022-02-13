5e567146d2da4908f2938ce0da1b113bd03b2e4d
Avdiji <avdiji.fitor98@gmail.com>
2022-02-13 14:21:05 +0100

    test commit

--- Scripts/run.py
+++ Scripts/run.py
@@ -5,3 +5,6 @@ single_logfile = "../Domain_List/Single_Domains_HTML/single_logfile.log"
 single_saveDirectory = "../Domain_List/Single_Domains_HTML"
 
 download_html_single(single_domainList_path, single_saveDirectory, single_logfile)
+
+
+#test
