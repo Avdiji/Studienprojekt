952ceae3a984038da2d4779fffe9f4c43f805dc7
Avdiji <avdiji.fitor98@gmail.com>
2022-02-16 20:40:01 +0100

    created class cleaning    added constructor    added method init_dict_soup    added method get_urlAppendix    added method get_cleaned_soup
    adjusted class wget
       adjusted method download_html_sub

--- Scripts/Helper/cleaning.py
+++ Scripts/Helper/cleaning.py
@@ -2,10 +2,62 @@ from bs4 import BeautifulSoup
 import os
 
 
-def get_HTML_Dict(html_path):
-    dict_result = {}
+# ----------------------------------------------------------------------------------------------------
+# Class is used to clean the downloaded html files, find and download subdomains
+# and clean the html file from all scripts and stylesheets
+# ----------------------------------------------------------------------------------------------------
+class Cleaning:
+    # ----------------------------------------------------------------------------------------------------
+    #                               ***** CONSTRUCTOR *****
+    # variables:
+    #       wget: wget: object to be able to download HTML - Files
+    #       dict_soup: Beautiful soup object to parse the HTML - Files
+    #
+    # Constructor initializes wget and dict_soup objects
+    # ----------------------------------------------------------------------------------------------------
+    def __init__(self, wget):
+        self.wget = wget
+        self.dict_soup = {}
 
-    dir_list = os.listdir(html_path)
-    for filenames in dir_list:
-        dict_result[filenames] = BeautifulSoup(open(f"{html_path}/{filenames}", "rb"), "html.parser")
-    return dict_result
+    # ----------------------------------------------------------------------------------------------------
+    # Function acts as a setter for self.dict_soup (initializes dict_soup with the filenames(key) and the
+    # corresponding BeautifulSoup4 Objects
+    # ----------------------------------------------------------------------------------------------------
+    def init_dict_soup(self):
+        dir_list = os.listdir(self.wget.html_path)
+        for filenames in dir_list:
+            bs = BeautifulSoup(open(f"{self.wget.html_path}/{filenames}", "rb"), "html.parser")
+            self.dict_soup[filenames] = bs
+
+    # ----------------------------------------------------------------------------------------------------
+    # parameter:
+    #       choir_name: name of the choir the appendix needs to be searched for
+    #
+    # Function returns the Appendix needed, to download the HTML-File with all the dates
+    # ----------------------------------------------------------------------------------------------------
+    def get_urlAppendix(self, choir_name):
+        result = ""
+
+        if choir_name == "eibach":
+            result = "/aktuelle-termine"
+        return result
+
+    # ----------------------------------------------------------------------------------------------------
+    # parameter:
+    #       choir_name: name of the choir
+    #       log_path: path of the logfile
+    #
+    # returns:
+    #       a BeautifulSoup4 object, that has been cleaned from the header, footer, css, and js
+    #
+    # Function returns a cleaned version of the HTML-File with the dates
+    # ----------------------------------------------------------------------------------------------------
+    def get_cleaned_soup(self, choir_name, log_path):
+        self.wget.download_html_sub(choir_name, self.get_urlAppendix(choir_name), log_path)
+        self.dict_soup[choir_name] = BeautifulSoup(open(f"{self.wget.html_path}/{choir_name}.html", "rb"),
+                                                   "html.parser")
+
+        result = self.dict_soup[choir_name].find("body")
+        for deco in result(["script", "style", "head", "footer"]):
+            deco.decompose()
+        return result
--- Scripts/run.py
+++ Scripts/run.py
@@ -1,14 +1,14 @@
-from Helper.wget import *
+from Helper.wget import WGet
+from Helper.cleaning import Cleaning
+
+url_path = "../Domain_List/Chore_URLS.txt"
+html_path = "../Domain_List/Chore_HTML"
+log_path = "../Domain_List/single_logfile.log"
 
 
+wget = WGet(url_path, html_path)
+wget.download_html(log_path)
 
-
-single_domainList_path = "../Domain_List/Chore_URLS.txt"
-single_logfile = "../Domain_List/single_logfile.log"
-single_saveDirectory = "../Domain_List/Chore_HTML"
-
-
-wget = WGet(single_domainList_path, single_saveDirectory)
-
-# wget.download_html(single_logfile)
-wget.download_html_sub('buchenbuehl','index.php/termine2020', single_logfile)
+clean = Cleaning(wget)
+clean.init_dict_soup()
+clean.get_cleaned_soup("eibach", log_path)
