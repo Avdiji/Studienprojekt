532913eb4ab7cc3431be3991c59d708c394826ac
Avdiji <avdiji.fitor98@gmail.com>
2022-02-22 19:54:48 +0100

    created class nlp

--- Scripts/run.py
+++ Scripts/run.py
@@ -1,7 +1,7 @@
 from Helper.wget import WGet
 from Helper.cleaning import Cleaning
 from Helper.segmentation import Segmentation
-import os
+from Helper.nlp import NLP
 
 url_path = "../Domain_List/Chore_URLS.txt"
 log_path = "../Domain_List/single_logfile.log"
@@ -18,9 +18,14 @@ clean.init_dict_soup()
 
 segmentation = Segmentation(clean)
 
+nlp = NLP(segmentation)
+
+
+
+
 with open(url_path, "r") as f:
     for line in f:
-        print(line.split(" ")[0])
-        print(
-            f"\t{segmentation.get_dates_soup(line.split(' ')[0])}\n\t{segmentation.get_contacts_soup(line.split(' ')[0])}\n"
-        )
+        print(line.split(" ")[0], nlp.get_name_matcher(line.split(" ")[0]))
+        # print(
+        #     f"\t{segmentation.get_contacts_soup(line.split(' ')[0])}\n"
+        # )
